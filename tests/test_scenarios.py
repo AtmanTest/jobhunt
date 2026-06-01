d4004839b8053379188639fe3596828f809a4ce3
Atman <thasin@live.com>
2026-06-01 11:21:04 +0200

    ajout section Année Sabbatique dans cv.html (pays en premier)

--- tests/test_scenarios.py
+++
@@ -1,5 +0,0 @@
-"""Import all BDD scenarios from .feature files for pytest discovery."""
-from pytest_bdd import scenarios
-
-# Discover all .feature files in the features directory
-scenarios("features")
